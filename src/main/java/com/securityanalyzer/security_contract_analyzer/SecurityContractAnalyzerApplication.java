0098345879892b3126b0e597a407b9afd4a48993
shivsharanprakash <shivsharanp49@gmail.com>
2026-08-26 20:08:04 +0530

    initial commit

---
+++ src/main/java/com/securityanalyzer/security_contract_analyzer/SecurityContractAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.securityanalyzer.security_contract_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityContractAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityContractAnalyzerApplication.class, args);
+	}
+
+}
